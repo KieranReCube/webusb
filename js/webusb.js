9695ecd4e9d59997bf4d198ed09fa7c30cbb72b4
bitten 1up <seant9140@gmail.com>
2022-03-21 19:48:36 -0500

    test one

---
+++ js/webusb.js
@@ -0,0 +1,15 @@
+function connect(){
+    navigator.usb.requestDevice({ filters: [{  }] })
+    .then(device => {
+        console.log(device.productName);      // "Arduino Micro"
+        console.log(device.manufacturerName); // "Arduino LLC"
+    })
+    .catch(error => { console.error(error); });
+    navigator.usb.getDevices().then(devices => {
+        devices.forEach(device => {
+          console.log(device.productName);      // "Arduino Micro"
+          console.log(device.manufacturerName); // "Arduino LLC"
+        });
+    })
+    device.open();
+}
