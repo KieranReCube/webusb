746c73b40fce74136d8fd1fa4d978534e1d1934a
bitten 1up <seant9140@gmail.com>
2022-03-24 10:25:09 -0500

    update

--- js/webusb.js
+++ js/webusb.js
@@ -13,4 +13,29 @@ function connect(){
         });
     })
     device.open();
+    // Prompt user to select any serial port.
+    const port = await navigator.serial.requestPort();
+
+    // Wait for the serial port to open.
+    await port.open({ baudRate: 9600 });
+    const textDecoder = new TextDecoderStream();
+    const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+    const reader = textDecoder.readable.getReader();
+
+    // Listen to data coming from the serial device.
+    while (true) {
+        const { value, done } = await reader.read();
+        if (done) {
+            // Allow the serial port to be closed later.
+            reader.releaseLock();
+            break;
+        }
+        // value is a string.
+        draw(text);
+    }
+}
+function draw(text) {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    ctx.font = '48px serif';
+    ctx.strokeText(text, 10, 50);
 }
